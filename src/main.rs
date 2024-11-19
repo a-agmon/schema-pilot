0a116d624b769b786edda94095e999fade97bf8e
Alon <alon.agmon@gmail.com>
2024-11-19 10:04:30 +0200

    adding llm funcs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,54 @@
+use futures::{future, stream, StreamExt};
+use std::sync::Arc;
 mod llm;
 mod vecdb;
-use yaml_rust2::{YamlEmitter, YamlLoader};
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     init_tracing();
-    read_tables("assets/schemas.yaml")?;
+    let tables = read_tables_definitions("assets/schemas.yaml")?;
+    let table_taggings = generate_table_tagging(tables).await?;
+    table_taggings.iter().for_each(|tag| {
+        println!("\n---\n{}", tag);
+    });
     Ok(())
 }
 
-fn read_tables(path: &str) -> anyhow::Result<()> {
+fn read_tables_definitions(path: &str) -> anyhow::Result<Vec<String>> {
     // read the file from the assets folder
     let file = std::fs::read_to_string(path)?;
-    let docs = YamlLoader::load_from_str(&file)?;
-    let schema = docs[0].clone();
-    println!("{:?}", schema);
-    Ok(())
+    // split the file into lines
+    let tables = file
+        .split("\n\n")
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+    Ok(tables)
+}
+
+// the function receives a vector of table definitions and generates for each table
+// a string that is made of tags and strings that are used to tag the table and represent the table schema
+async fn generate_table_tagging(tables: Vec<String>) -> anyhow::Result<Vec<String>> {
+    let prompt_str = r#"
+    The following is a database table definition:
+    {table_definition}
+    Instructions:
+    Generate a string that is made of tags and strings that are used to tag the table and represent the table schema.
+    "#;
+    let local_llm = Arc::new(llm::LlmClient::new("http://localhost", 11434));
+
+    let results = stream::iter(tables)
+        .then(|table| {
+            let llm = local_llm.clone();
+            let prompt = prompt_str.replace("{table_definition}", &table);
+            async move {
+                llm.generate_text(llm::LlmModel::Llama323b, &prompt)
+                    .await
+                    .unwrap()
+            }
+        })
+        .collect::<Vec<_>>()
+        .await;
+    Ok(results)
 }
 
 fn init_tracing() {
